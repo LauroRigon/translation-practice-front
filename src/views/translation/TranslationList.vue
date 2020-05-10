47f7408f7c5e3793c76f99d6166eec29bf86b2fb
laurorigon <laurorigon@gmail.com>
2020-05-10 10:42:27 -0300

    Initial commit

---
+++ src/views/translation/TranslationList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Listagem de Translations</h1>
+    {{ sharedState.translations }}
+  </div>
+</template>
+
+<script>
+import store from "@/store";
+
+function fetchTranslations(next) {
+  store.fetchTranslations()
+  .then(() => {
+    next()
+  });
+}
+
+export default {
+  name: "TranslationList",
+
+  data() {
+    return {
+      sharedState: store.state,
+    }
+  },
+
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    fetchTranslations(next);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/translation/TranslationShow.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "TranslationShow"
+}
+</script>
+
+<style scoped>
+
+</style>
